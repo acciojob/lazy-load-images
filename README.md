2fbcc58a9e6cf80cc45fe8941affe5acddacfd7f
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-07-05 16:45:38 +0530

    ques

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Acciojob Assignment Solution Template Repo
+# Lazy Load Images
 
-# Instructions 
+You are building a web page that contains a large number of images. To improve the page's performance, you decide to implement lazy loading of images so that they only load when they are in the viewport. Write a JavaScript function that detects when an image is in the viewport and asynchronously loads it.
+
+## Instructions
+
+1. Basic boilerplate is already given to you with CSS and HTML.
+2. You just have to modify the `script.js` file
+3. Whenever image is loaded on screen, change the `data-loaded` attribute to `true` in each img tag. Also remove the class `lazy`
--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-//your JS code here. If required.
+const imageContainer = document.getElementById("image-container");
+const imageCount = 30;
+
+const createImage = (src) => {
+  const imageWrapper = document.createElement("div");
+  imageWrapper.className = "image";
+
+  const img = document.createElement("img");
+  img.setAttribute("data-src", src);
+  img.className = "lazy";
+
+  const placeholder = document.createElement("div");
+  placeholder.className = "placeholder";
+
+  imageWrapper.appendChild(img);
+  imageWrapper.appendChild(placeholder);
+  return imageWrapper;
+};
+
+for (let i = 0; i < imageCount; i++) {
+  const imageUrl = `https://picsum.photos/seed/${Math.random()}/300/300`;
+  const image = createImage(imageUrl);
+  imageContainer.appendChild(image);
+}
+
+// Do not modify the above code
+// The above code just renders 30 random images on screen
+// Write code below and modify the functions
+
+// This function should set the src of images
+// The src for each img element is given in the data-src attribute
+const loadImage = (img) => {};
+
+// You can set the data-loaded attribute true here
+const onImageLoad = (img) => {};
+
+// Check if image is visible on screen
+const isInViewport = (element) => {};
+
+// Load all the images visible on screen
+const lazyLoadImages = () => {};
+
+window.addEventListener("scroll", lazyLoadImages);
+window.addEventListener("resize", lazyLoadImages);
+window.addEventListener("DOMContentLoaded", lazyLoadImages);
